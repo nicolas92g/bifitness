ea3c7a142000ab27c1f3b56f3a46a893f08c5381
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2023-01-05 15:55:19 +0100

    Update entrainement.php

--- pages/entrainement.php
+++ pages/entrainement.php
@@ -3,12 +3,14 @@ include ("../source/siteStructure.php");
 
 $content = "
 <section class='pageContainer'>
+    <h2>Programme Homme</h2>
     <img src='../assets/programmeGratuit1.png' alt='programme Gratuit page 1' class='freeProgramme'>
     <a href='programmes/pdfs/1%20nutrition%20femme%20masse%20-%2045KG.pdf' download='programmeGratuitFemme.pdf'>
-        Cliquez ici pour télécharchez le programme pour les femmes gratuitement
+        Télécharger le programme
     </a>
+    <h2>Programme Femme</h2>
     <a href='programmes/pdfs/Homme%20masse%205X%20programme%20prise%20de%20muscle.pdf' download='programmeGratuitHomme.pdf'>
-        Cliquez ici pour télécharchez le programme pour les hommes gratuitement
+        Télécharger le programme
     </a>
 </section>
 ";
