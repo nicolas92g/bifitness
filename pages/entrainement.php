4c5f0f572297378debc8595d6848f6be6d16af4f
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2023-01-05 15:22:20 +0100

    modif

--- pages/entrainement.php
+++ pages/entrainement.php
@@ -3,9 +3,7 @@ include ("../source/siteStructure.php");
 
 $content = "
 <section class='pageContainer'>
-    <iframe src='programmes/pdfs/femme%20programme%204%20X%20SEMAINE.pdf' class='pdfReader'></iframe>
-    <!--<H2>Programme Femme</H2>
-    <iframe src='programmes/pdfs/femme%20programme%204%20X%20SEMAINE.pdf'></iframe>-->
+    <img src='../assets/programmeGratuit1.png' alt='programme Gratuit page 1' class='freeProgramme'>
 </section>
 ";
 
