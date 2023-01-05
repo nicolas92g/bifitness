90ebf19cfd557509d8ea56ea679efa294a29dc9c
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2023-01-05 15:51:30 +0100

    modif

--- pages/entrainement.php
+++ pages/entrainement.php
@@ -4,6 +4,12 @@ include ("../source/siteStructure.php");
 $content = "
 <section class='pageContainer'>
     <img src='../assets/programmeGratuit1.png' alt='programme Gratuit page 1' class='freeProgramme'>
+    <a href='programmes/pdfs/1%20nutrition%20femme%20masse%20-%2045KG.pdf' download='programmeGratuitFemme.pdf'>
+        Cliquez ici pour télécharchez le programme pour les femmes gratuitement
+    </a>
+    <a href='programmes/pdfs/Homme%20masse%205X%20programme%20prise%20de%20muscle.pdf' download='programmeGratuitHomme.pdf'>
+        Cliquez ici pour télécharchez le programme pour les hommes gratuitement
+    </a>
 </section>
 ";
 
